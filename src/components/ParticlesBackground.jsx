1aea06a0daeb15ff4f4d9b0b1a793af1c135d976
ivan levy <ivo.levy03@gmail.com>
2026-01-31 23:55:39 -0300

    ui

--- src/components/ParticlesBackground.jsx
+++ src/components/ParticlesBackground.jsx
@@ -138,12 +138,12 @@ export const ParticlesBackground = () => {
       ref={canvasRef}
       className="particles-background"
       style={{
-        position: "fixed",
+        position: "absolute",
         top: 0,
         left: 0,
         width: "100%",
         height: "100%",
-        zIndex: -1,
+        zIndex: 1,
         backgroundColor: "transparent",
         pointerEvents: "none"
       }}
