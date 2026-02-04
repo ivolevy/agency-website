cd3ebbbd1272320e980c71882cffa913b79880f0
ivan levy <ivo.levy03@gmail.com>
2026-02-04 00:07:45 -0300

    form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,12 @@ export const Header = (props) => {
   return (
     <>
       <header className="header" id="header">
+        <div className="mesh-container">
+          <div className="aurora-blob blob-1"></div>
+          <div className="aurora-blob blob-2"></div>
+          <div className="aurora-blob blob-3"></div>
+          <div className="glass-overlay"></div>
+        </div>
         <div className="nav">
           <NavComponent />
         </div>
