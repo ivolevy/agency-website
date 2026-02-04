e496bc3b25372c3919a24a71c72125f048bee37c
ivan levy <ivo.levy03@gmail.com>
2026-02-03 23:56:16 -0300

    form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,11 @@
 import { NavComponent } from "./Nav";
 import "../assets/styles/header.css";
 import { Hero } from "../assets/comps/Hero";
-import { ParticlesBackground } from "./ParticlesBackground";
 
 export const Header = (props) => {
   return (
     <>
       <header className="header" id="header">
-        <ParticlesBackground />
         <div className="nav">
           <NavComponent />
         </div>
@@ -31,4 +29,4 @@ export const Header = (props) => {
 
 
 
- 
+
